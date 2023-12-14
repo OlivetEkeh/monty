169e18ec1940484cff7d124322a4b1f7074ef2bc
ADENUGA Sunday <135983382+sunda12345667@users.noreply.github.com>
2023-12-14 22:12:40 +0100

    enqueue.c

---
+++ enqueue.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * enqueue - the Function that add node to the bottom of the queue.
+ * @head: the Head
+ * @n: new value
+ * Return: Null
+ */
+void enqueue(stack_t **head, int n)
+{
+	stack_t *newNode, *currentNode;
+
+	currentNode = *head;
+
+	newNode = malloc(sizeof(stack_t));
+	if (newNode == NULL)
+	{
+		fprintf(stderr, "Error: Memory allocation failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	newNode->n = n;
+	newNode->next = NULL;
+
+	if (currentNode)
+	{
+		while (currentNode->next)
+			currentNode = currentNode->next;
+	}
+
+	if (!currentNode)
+	{
+		*head = newNode;
+		newNode->prev = NULL;
+	}
+	else
+	{
+		currentNode->next = newNode;
+		newNode->prev = currentNode;
+	}
+}
