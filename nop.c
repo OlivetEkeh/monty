d7d5140aaaecf031fb78df6bb2da9a00cc67bb45
ADENUGA Sunday <135983382+sunda12345667@users.noreply.github.com>
2023-12-14 22:16:44 +0100

    nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - this function does nothing
+ * @head: the Head of the stack
+ * @line_number: Line number integer
+ * Return: Null
+ */
+void _nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+}
